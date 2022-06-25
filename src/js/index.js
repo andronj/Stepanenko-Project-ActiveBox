d6fa1e3bc02e3dd402abf217de3551e082c3a8ed
andronj <andstepanenko@tut.by>
2022-06-25 15:31:57 +0300

    create header

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 console.log('Aloha')
+
+// Меню бургер
+
+const iconMenu = document.querySelector('.header-menu-burger');
+if (iconMenu){
+    const menuBody = document.querySelector('.header-menu');
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
